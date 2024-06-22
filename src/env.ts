44e422b4653c40e2490ec2ffe64b3f33b2aba61d
Igor Lins e Silva <4753812+igorls@users.noreply.github.com>
2024-06-22 13:08:25 -0300

    fix logo

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   eosioNodeUrl: 'https://spring.hyperion.eosrio.io',
   hyperionApiUrl: 'https://spring.hyperion.eosrio.io',
-  logo: 'https://monitor.spring.jungletestnet.io/imgs/jungle_logo_with_text.png',
+  logo: 'https://monitor.spring-beta2.jungletestnet.io/imgs/jungle_logo_with_text.png',
   systemContract: 'eosio',
   userResourcesTable: 'userres'
 };
