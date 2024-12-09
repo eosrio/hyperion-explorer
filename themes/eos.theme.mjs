c0947a1358c2e490ed8fccad792685f890037641
Dominique Deschatre <43499065+domiscd@users.noreply.github.com>
2024-12-09 18:50:52 -0300

    Create eos.theme.mjs

---
+++ themes/eos.theme.mjs
@@ -0,0 +1,12 @@
+// Description: Theme for Jungle Testnet
+
+globalThis.themeData = {
+  "--primary": "#fba219",
+  "--accent": "#2fbce1",
+  "--faded": "#a8968a",
+  "--main-background": "#f1f1f1",
+  "--card-bg-gradient": "linear-gradient(115deg, rgb(255 255 255 / 70%) 0%, rgb(255 255 255 / 90%) 50%, rgb(255 255 255 / 70%) 100%)",
+  "--table-top-bg-gradient": "linear-gradient(115deg, rgb(255 255 255 / 60%) 0%, rgb(255 255 255 / 90%) 87%, rgb(255 255 255 / 60%) 130%), var(--main-background)",
+  "--main-text": "#141519",
+  "--footer-text": "#767676"
+}
